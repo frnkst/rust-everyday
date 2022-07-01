ac190424c563b3ba976a54a8cc92115ee9c69ca6
Frank <frank@void0.ch>
2022-07-01 15:15:46 +0200

    Rust alert

---
+++ src/bin/20_rust_alert_on_webpage_fail.rs
@@ -0,0 +1,34 @@
+use std::fmt::Error;
+use notify_rust::Notification;
+use headless_chrome::{Browser, protocol::page::ScreenshotFormat};
+
+fn browse_wikipedia() -> Result<(), failure::Error> {
+    let browser = Browser::default()?;
+    let tab = browser.wait_for_initial_tab()?;
+
+    tab.navigate_to("https://www.wikipedia.org")?;
+    tab.wait_for_element("input#searchInput")?.click()?;
+    tab.type_str("WebKit")?.press_key("Enter")?;
+    tab.wait_for_element("#firstHeading")?;
+
+    if tab.get_url().ends_with("WebKitA") {
+        return Ok(())
+    }
+
+    return Err(failure::err_msg("bla"))
+}
+
+fn main() {
+    let message: &str;
+
+    match browse_wikipedia() {
+        Ok(_) => message = "Wikipedia is ok",
+        Err(_) => message = "Wikipedia is down"
+    }
+
+    Notification::new()
+        .summary("Wikipedia")
+        .body(message)
+        .icon("firefox")
+        .show().expect("Could not show notification");
+}
